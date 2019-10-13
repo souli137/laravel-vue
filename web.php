f483ddfee4174170d27e7431ce036886eda0c6da
souli137 <40316605+souli137@users.noreply.github.com>
2019-10-13 16:33:12 +0200

    Update web.php

--- web.php
+++ web.php
@@ -9,11 +9,11 @@ Route::get('/', function () {
 Route::prefix('auth')->group(function () {
   Route::get('init', 'AppController@init');
 
-  Route::post('login', 'AppController@login');
+  Route::post('login', 'AppController@login'); //envoyer la requete d'authentification
   Route::post('register', 'AppController@register');
-  Route::post('logout', 'AppController@logout');
+  Route::post('logout', 'AppController@logout'); //envoyer la requete de deconnexion
 
-  Route::post('/update/user', 'AppController@update');
+  Route::post('/update/user', 'AppController@update'); //envoyer la requete de la mise à jour du profil utilisateur
   Route::post('/update/password/user', 'AppController@changePassword');
 
 

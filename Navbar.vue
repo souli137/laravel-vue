195cdc76ef11eb3153ac2150238f7e35c021c6c0
souli137 <40316605+souli137@users.noreply.github.com>
2019-10-13 16:40:34 +0200

    Update Navbar.vue

--- Navbar.vue
+++ Navbar.vue
@@ -19,15 +19,7 @@
     </ul>
     <ul class="navbar-nav ml-auto">
      <li class="nav-item dropdown">
-        <a 
-          class="nav-link dropdown-toggle" 
-          href="#" 
-          id="navbarDropdown" 
-          role="button" 
-          data-toggle="dropdown"  
-          aria-haspopup="true" 
-          aria-expanded="false"
-        >{{ app.user ? app.user.name : 'Accounté }}</a>
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ app.user ? app.user.name : 'Accounté' }}</a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
           <div v-if="!app.user">
           <router-link to="/login" class="dropdown-item">Login</router-link>
